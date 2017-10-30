998bbd1c5c0772514e8ed8aff6eb37d66814fa4e
MLJJ <acdc.ml.999@gmail.com>
2017-10-29 20:49:04 -0600

    Se renombro el controlador y vista del inicio de sesion, al igual que algunas variables que se definen automaticamente dentro de este controlador; ademas, se implemento las condiciones para el inicio de sesion y la imlemetacion de los metodos pertenecientes a este (interfaz: campos invalidos, controlador: autentificar usuario). En cuanto al registro de usuario se implemetaron los metodos restringen el numero de caracteres para el nombre de usuario y la restriccion de colocacion de espacios para la creacion de una contraseña; ademas, de los metodos y condiciones pertanecientes a este (interfaz: campos invalidos, controlador: registrarUsuario).

---
+++ 5Linea/src/cincolinea/controlador/FXMLInicioSesionController.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cincolinea.controlador;
+
+import cincolinea.Main;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import conexion.Cliente;
+import java.net.URL;
+import java.rmi.RemoteException;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Adrian Bustamante Z
+ */
+public class FXMLInicioSesionController implements Initializable {
+    
+    @FXML
+    private Label labelContraseña;
+    @FXML
+    private Label labelUsuario;
+    @FXML
+    private Label labelTitle;
+    @FXML
+    private JFXButton btnSalir;
+    @FXML
+    private JFXButton btnIngresar;
+    @FXML
+    private Hyperlink linkRegistro;
+    @FXML
+    private JFXButton translateButton;
+    
+    private boolean estaEspañol;
+    private ResourceBundle idioma;
+    private Main main;
+    private Cliente conexion;
+    @FXML
+    private JFXTextField txtNombreUsuario;
+    @FXML
+    private JFXPasswordField txtContrasena;
+    
+    @FXML
+    private void cambiarIdioma(ActionEvent event) {
+        if (estaEspañol) {
+            cambiarComponentesIngles();
+        } else {
+            cambiarComponentesEspañol();
+        }
+    }
+
+    @FXML
+    private void desplegarRegistroUsuario(javafx.event.ActionEvent event){
+        main.desplegarRegistrarUsuario(idioma);
+    }
+    
+    @FXML
+    private void ingresarMenuPrincipal(ActionEvent event) {
+        
+        if(conexion.autenticarCuenta(txtNombreUsuario.getText(),txtContrasena.getText())){
+            main.desplegarMenuPrincipal(idioma);
+        }else{
+            System.out.println("Error en los datos del usuario");
+        }
+        
+        
+    }
+    
+    public void setMain(Main main) {
+        this.main = main;
+    }
+
+    private void cambiarComponentesEspañol() {
+        idioma = ResourceBundle.getBundle("cincolinea/resources/Bundle_es_MX");
+        labelTitle.setText(idioma.getString("labelTitle"));
+        labelContraseña.setText(idioma.getString("labelContraseña"));
+        labelUsuario.setText(idioma.getString("labelUsuario"));
+        btnIngresar.setText(idioma.getString("btnIngresar"));
+        btnSalir.setText(idioma.getString("btnSalir"));
+        linkRegistro.setText(idioma.getString("linkRegistro"));
+        estaEspañol = true;
+    }
+    
+    private void cambiarComponentesIngles() {
+        idioma = ResourceBundle.getBundle("cincolinea/resources/Bundle_en_US");
+        labelTitle.setText(idioma.getString("labelTitle"));
+        labelContraseña.setText(idioma.getString("labelContraseña"));
+        labelUsuario.setText(idioma.getString("labelUsuario"));
+        btnIngresar.setText(idioma.getString("btnIngresar"));
+        btnSalir.setText(idioma.getString("btnSalir"));
+        linkRegistro.setText(idioma.getString("linkRegistro"));
+        estaEspañol = false;
+    }
+    
+    private void inicializarComponentesPorDefecto(ResourceBundle bundleElegido) {
+        labelTitle.setText(bundleElegido.getString("labelTitle"));
+        labelContraseña.setText(bundleElegido.getString("labelContraseña"));
+        labelUsuario.setText(bundleElegido.getString("labelUsuario"));
+        btnIngresar.setText(bundleElegido.getString("btnIngresar"));
+        btnSalir.setText(bundleElegido.getString("btnSalir"));
+        linkRegistro.setText(bundleElegido.getString("linkRegistro"));
+        idioma = bundleElegido;
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle idioma) {
+        try {
+            conexion = new Cliente();
+        } catch (RemoteException ex) {
+            Logger.getLogger(FXMLInicioSesionController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        if (idioma != null) {
+            inicializarComponentesPorDefecto(idioma);
+        } else {
+            cambiarComponentesEspañol();
+        }
+        translateButton.setStyle("-fx-background-image: url('cincolinea/imagenes/language.png');"
+                + "-fx-background-position: center center; -fx-background-repeat: stretch; -fx-background-size: 35px 35px 35px 35px;");
+        
+    }
+    
+}
--- 5Linea/src/cincolinea/controlador/FXMLRegistrarUsuarioController.java
+++ 5Linea/src/cincolinea/controlador/FXMLRegistrarUsuarioController.java
@@ -2,12 +2,20 @@ package cincolinea.controlador;
 
 import cincolinea.Main;
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import conexion.Cliente;
 import java.net.URL;
+import java.rmi.RemoteException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 
 /**
  * FXML Controller class
@@ -29,11 +37,20 @@ public class FXMLRegistrarUsuarioController implements Initializable {
 
     private ResourceBundle idioma;
     private Main main;
+    private Cliente conexion;
+    @FXML
+    private ImageView imagen;
+    @FXML
+    private JFXPasswordField tfReContrasena;
+    @FXML
+    private JFXPasswordField tfContrasena;
+    @FXML
+    private JFXTextField tfNombreUsuario;
 
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        idioma = rb;
-        if (idioma != null) {
+    public void initialize(URL url, ResourceBundle idioma) {
+        this.idioma = idioma;
+        if (this.idioma != null) {
             iniciarIdiomaComponentes();
         }
     }
@@ -46,12 +63,55 @@ public class FXMLRegistrarUsuarioController implements Initializable {
         btnRegistrar.setText(idioma.getString("btnRegistrar"));
         btnCancelar.setText(idioma.getString("btnCancelar"));
         labelNombreUsuario.setText(idioma.getString("labelUsuario"));
-        labelContrasena.setText(idioma.getString("labelContrasena"));
+        labelContrasena.setText(idioma.getString("labelContraseña"));
         labelReContrasena.setText(idioma.getString("labelReContrasena"));
     }
 
     @FXML
-    private void regresarMenuPrincipal(ActionEvent event) {
+    private void regresarMenuPrincipal(ActionEvent evento) {
         main.desplegarMenuPrincipal(idioma);
+    }
+    
+    @FXML
+    private void accionRegistrarUsuario(ActionEvent evento){
+        //3
+        if(tfNombreUsuario.getText().isEmpty() || tfContrasena.getText().isEmpty() || tfReContrasena.getText().isEmpty()){
+            System.out.println("Hay campos vacios");
+        }else{
+            try {
+                conexion = new Cliente();
+            } catch (RemoteException ex) {
+                //QUITAR
+                Logger.getLogger(FXMLRegistrarUsuarioController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            //4
+            if(tfContrasena.getText() == tfReContrasena.getText()){
+                //5
+                if (conexion.registrarUsuario(tfNombreUsuario.getText(), tfContrasena.getText())) {
+                    System.out.println("Usuario guardado");
+                } else {
+                    System.out.println("No se guardo");
+                }
+            }else{
+                System.out.println("La contraseña no conduerda");
+            }
+        }
+        
+    }
+    
+    @FXML
+    private void restringirNumeroDeCaracteres(KeyEvent evento){
+        if(tfNombreUsuario.getText().length() >= 20){
+            evento.consume();
+        }
+    }
+    
+    @FXML   
+    private void restringirEspacios(KeyEvent evento){
+        char caracter = evento.getCharacter().charAt(0);
+        
+        if(caracter == ' '){
+            evento.consume();
+        }
     }
 }

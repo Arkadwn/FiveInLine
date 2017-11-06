500500df1f109b6fc6f97413d86e38bd8973f391
Adrian Bustamante <arkadwn@gmail.com>
2017-11-06 17:18:04 -0600

    Modificación del diseño del tablero y corrección de servidor

--- CincoLineaServidor/servidornode/servidor.js
+++ CincoLineaServidor/servidornode/servidor.js
@@ -1,9 +1,34 @@
 var io=require('socket.io')(8000);
 
+
+
 io.on('connection', function(socket, mensaje) {
 	console.log('Se ha conectado: '+ mensaje);
-	socket.on('saludoServidor', function(mensaje){
-		socket.broadcast.emit('saludoCliente', mensaje);
+	socket.on('peticionCreacionPartida', function(ip){
+		//socket.broadcast.emit('saludoCliente', mensaje);
+		//codigo para guardar ipAnfritiones
 	});
+	socket.on('peticionEnlacePartida',function(ip){
+		socket.send.emit('jugadores', jugadores);
+	});
+
+	socket.on('emparejar',function(ipAnfitrion,ipInvitdo){
+		//codigo para emparejar
+	});
+
+	socket.on('jugada',function(ip, jugada){
+		//revisar conexion
+
+		socket.broadcast.emit(jugada);
+		//envio de tiempo de espera para jugada
+	});
+
+	socket.on('disconnect',function{
+		io.emit('user disconnect');
+	});
+
+
+
+
 });
 

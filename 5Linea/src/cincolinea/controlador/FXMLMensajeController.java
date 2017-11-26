b4f8b399e5e37b4cadb5d693db2f8e6786f68682
MLJJ <acdc.ml.999@gmail.com>
2017-11-26 16:01:07 -0600

    Se agregaron los mensajes de retroalimentacion para el usuario en los Bundle de español e ingles, se creo un controlador para mandar mensajes de retroalimentacion (informacion, advertencia y decision) como metodos estaticos al usuario

---
+++ 5Linea/src/cincolinea/controlador/FXMLMensajeController.java
@@ -0,0 +1,47 @@
+package cincolinea.controlador;
+
+import java.util.Optional;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author Miguel Leonardo Jimenez Jimenez
+ */
+public class FXMLMensajeController {
+    public static void mensajeAdvertencia(String mensaje){
+        Alert ventana = new Alert(Alert.AlertType.WARNING);
+        ventana.setTitle(null);
+        ventana.setHeaderText(mensaje);
+        ventana.setContentText(null);
+        ventana.initStyle(StageStyle.UTILITY);
+        ventana.showAndWait();
+    }
+    
+    public static void mensajeInformacion(String mensaje){
+        Alert ventana = new Alert(Alert.AlertType.INFORMATION);
+        ventana.setTitle(null);
+        ventana.setHeaderText(null);
+        ventana.setContentText(mensaje);
+        ventana.initStyle(StageStyle.UTILITY);
+        ventana.showAndWait();
+    }
+    
+    public static boolean mensajeDecisio(String mensaje){
+        boolean validacion = true;
+        
+        Alert ventana = new Alert(Alert.AlertType.CONFIRMATION);
+        ventana.setTitle(null);
+        ventana.setHeaderText(null);
+        ventana.setContentText(mensaje);
+        ventana.initStyle(StageStyle.UTILITY);
+        
+        
+        Optional<ButtonType> resultado = ventana.showAndWait();
+        
+        validacion = resultado.get() == ButtonType.OK;
+        
+        return validacion;
+    }
+}
